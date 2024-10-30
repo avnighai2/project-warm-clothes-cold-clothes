c38b0c2a661094b7644aa8b36add0b1474eee564
avnighai2 <avnighai2@gmail.com>
2024-10-30 19:50:00 -0400

    cold clothes warm clothes

---
+++ project.py
@@ -0,0 +1,8 @@
+# Input: Temperature from user
+temperature = float(input("Enter the current temperature in Celsius: "))
+
+# Simple decision for clothing
+if temperature >= 20:
+    print("You can wear light clothes!")
+else:
+    print("Wear something warm to stay cozy!")
